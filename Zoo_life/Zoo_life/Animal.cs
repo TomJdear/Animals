90fd25a19855bd36b8640cb451a9303b7a82bea9
RajDaddy <rajkud@hotmail.co.uk>
2017-01-03 14:37:49 +0000

    Add files via upload

---
+++ Zoo_life/Zoo_life/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo_life
+{
+    public class Animal
+    {
+        public bool HasLegs { get; private set; }
+        public int Age { get; set; }
+        public int YearsAtZoo { get; set; }
+        public bool IsMale { get; set; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("Noises like an animal!");
+        }
+        public override string ToString()
+        {
+            return "I'm an animal!";
+        }
+        public virtual bool ReadyToBreed(int age, bool isMale)
+        {
+            return false;
+        }
+    }
+}
---
+++ Zoo_life/Zoo_life/Reptile_Species.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo_life
+{
+    public class Reptile_Species : Animal
+    {
+        public bool coldBlooded { get; set; }
+        public bool hasFangs { get; set; }
+        public bool Venomous { get; set; }
+
+        private Reptile_Species()
+        {
+            bool coldBlooded = true;
+        }
+
+        public Reptile_Species(bool hasfangs, bool venomous)
+        {
+            hasFangs = hasfangs;
+            Venomous = venomous;
+        }
+    }
+}
