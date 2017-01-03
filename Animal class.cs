2588df76f649c694fca524fd0ea90b5090f1abcf
TomJdear <Tomjdear97@gmail.com>
2017-01-03 13:57:54 +0000

    Add files via upload

---
+++ Animal class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTASDF_Animal_Kingdom_andy
+{
+    public class Animal
+    {
+        public bool HasLegs { get; private set; }
+        public int Age { get; set; }
+        public int YearsAtZoo { get; set; }
+        public bool IsMale { get; set; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("Noises like an animal!");
+        }
+        public override string ToString()
+        {
+            return "I'm an animal!";
+        }
+
+    }
+}
