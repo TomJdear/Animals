94f851b407252411b30d4c3d42ea8b77e7262e5b
TomJdear <Tomjdear97@gmail.com>
2017-01-03 14:37:03 +0000

    Add files via upload

---
+++ Bird.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Class_Github
+{
+    class Bird: Animal
+    {
+        public int WingSpan { get; private set; }
+        public bool CanFly { get; private set; }
+        public bool IsCarnivore { get; private set; }
+        public bool IsBirdOfprey { get; private set; }
+        public bool IsNocturnal { get; private set; }
+    }
+}
